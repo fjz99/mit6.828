ab3b4b6f1a13e0919a6282eaae059b7d771daf94
fujiazheng <1358925318@qq.com>
2022-11-23 13:02:12 +0000

    lab3A1

---
+++ inc/env.h
@@ -0,0 +1,65 @@
+/* See COPYRIGHT for copyright information. */
+
+#ifndef JOS_INC_ENV_H
+#define JOS_INC_ENV_H
+
+#include <inc/types.h>
+#include <inc/trap.h>
+#include <inc/memlayout.h>
+
+typedef int32_t envid_t; // 有符号数
+
+// An environment ID 'envid_t' has three parts:
+//
+// +1+---------------21-----------------+--------10--------+
+// |0|          Uniqueifier             |   Environment    |
+// | |                                  |      Index       |
+// +------------------------------------+------------------+
+//                                       \--- ENVX(eid) --/
+//
+// The environment index ENVX(eid) equals the environment's index in the
+// 'envs[]' array.  The uniqueifier distinguishes environments that were
+// created at different times, but share the same environment index.
+//
+// All real environments are greater than 0 (so the sign bit is zero).
+// envid_ts less than 0 signify errors.  The envid_t == 0 is special, and
+// stands for the current environment.
+// JOS没有slab，所以使用另一种方式fast！
+// 首先直接分配所有的进程struct的array，然后用进程id的低位10位表示array的idx，所以arr只能有1024个
+// 而且我们的内存分配器只能分配PAGE大小的内存，所以单个Env结构不方便分配
+
+#define LOG2NENV		10
+#define NENV			(1 << LOG2NENV)
+#define ENVX(envid)		((envid) & (NENV - 1))
+
+// Values of env_status in struct Env
+enum {
+	ENV_FREE = 0,
+	ENV_DYING,
+	ENV_RUNNABLE,
+	ENV_RUNNING,
+	ENV_NOT_RUNNABLE
+};
+
+// Special environment types
+enum EnvType {
+	ENV_TYPE_USER = 0,
+};
+
+// 进程描述符
+struct Env {
+	// Saved registers
+	// 即进行系统调用或者进程切换的时候的上下文，中断不会自动保存上下文，需要在中断中立程序中手动保存
+	struct Trapframe env_tf;	
+	struct Env *env_link;		// Next free Env
+	envid_t env_id;			// Unique environment identifier
+	envid_t env_parent_id;		// env_id of this env's parent
+	enum EnvType env_type;		// Indicates special system environments
+	unsigned env_status;		// Status of the environment
+	uint32_t env_runs;		// Number of times environment has run
+
+	// Address space
+	pde_t *env_pgdir;		// Kernel virtual address of page dir
+};
+
+#endif // !JOS_INC_ENV_H
